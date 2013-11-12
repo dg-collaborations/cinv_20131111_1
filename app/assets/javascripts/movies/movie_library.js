f824851898fa49f40eee75cae2c5f28de900e0a7
DecisionGrid <decisiongrid@DecisionGrids-MacBook-Air.local>
2013-11-11 18:44:25 -0600

    Pushing new changes

--- app/assets/javascripts/movies/movie_library.js
+++ app/assets/javascripts/movies/movie_library.js
@@ -1,4 +1,33 @@
 (function(targetNamespace){
+	function MovieLibrary(a_Movies) {
+		this.movies = a_Movies;
+	}
+
+	var proto = MovieLibrary.prototype;
+
+	// return all pixar movies
+	proto.allMoviesByStudio = function(studio_const) {
+		var return_array = [];
+		for(var i = 0; i<this.movies;i++) {
+			curr = this.movies[i];
+
+			if (curr.studio === studio_const) {
+				return_array.push(curr);
+			}
+		}
+		return return_array;
+	}
+
+	proto.allPixarMovies = function() {
+		return allMoviesByStudio(STUDIOS.PIXAR);
+	}
+
+	proto.allPixarOrDisneyMovies = function() {
+		// TOD
+	}
+
+	proto.allMoviesNotPublishedByPixar = function() {
+
+	}
 
 })(this);
-
