a2c9007ee3602028ebd6b89ad5e6710eb83cf385
Jean-Paul S. Boodhoo <jp@developwithpassion.com>
2013-11-11 18:29:39 -0700

    Updated

--- app/assets/javascripts/movies/movie_library.js
+++ app/assets/javascripts/movies/movie_library.js
@@ -5,43 +5,17 @@
 
 	var proto = MovieLibrary.prototype;
 
-	// return all movies from studio
-	proto.allMoviesByStudio = function(studio_const) {
-		var return_array = [];
-
-		this.movies.forEach(
-			function() {
-				if (curr.studio === studio_const) {
-					return_array.push(curr);
-				}
-			}
-		)
-
-		return_array;
-	}
-
-	// return all movies not from studio
-	proto.allMoviesNotByStudio = function(studio_const) {
-		var return_array = [];
-		for(var i = 0; i<this.movies;i++) {
-			curr = this.movies[i];
-
-			if (curr.studio !== studio_const) {
-				return_array.push(curr);
-			}
-		}
-		return return_array;
-	}	
 
 	proto.allPixarMovies = function() {
-		return this.allMoviesByStudio(STUDIOS.PIXAR);
+		return this.movies.filter(function(){
+      this.studio === STUDIOS.PIXAR;
+    });
 	}
 
 	proto.allPixarOrDisneyMovies = function() {
-		pixar = this.allMoviesByStudio(STUDIOS.PIXAR);
-		disney = this.allMoviesByStudio(STUDIOS.DISNEY);
-
-		return pixar.concat(disney);
+		return this.movies.filter(function(){
+      this.studio === STUDIOS.PIXAR || this.studio === STUDIOS.DISNEY;
+    })
 	}
 
 	proto.allMoviesNotPublishedByPixar = function() {
--- app/assets/javascripts/utility/array_extensions.js
+++ app/assets/javascripts/utility/array_extensions.js
@@ -1,22 +1,20 @@
 (function(targetNamespace){
+
   Array.prototype.forEach = function(func){
-  	for(var i = 0; i<this.length; i++) {
+  	for(var i = 0; i <this.length; i++) {
   		func.call(this[i]);
   	}
   };
 
   Array.prototype.filter = function(matcher) {
-  		var self = this;
-  		var return_array = [];
+    var results = [];
 
-		this.forEach(
-			function() {
+		this.forEach(function(){
 				if (matcher.call(this)) {
-					return_array.push(this);
-				}
-			}
-		);
+					results.push(this);
+				} 
+      });
 
-		return_array;
+		return results;
   }
 })(this)
